b0aef28277aa9ec00c06617ce5f0f163c8688dea
YasinDehfuli <ydehfuli@gmail.com>
2023-10-14 11:07:50 +0330

    Refactored HomeView.vue to import TheWelcome component from '@/components/TheWelcome.vue', added a reactive message variable, set a timeout to change the message after 2 seconds, and added an alert function on button click.

--- Vue-Learn-Part-2/src/views/HomeView.vue
+++ Vue-Learn-Part-2/src/views/HomeView.vue
@@ -1,9 +1,31 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from '@/components/TheWelcome.vue';
+
+
+// onMounted(() =>{
+//     alert('HIIII')
+// });
+
+let message = $ref('hellow world');
+
+setTimeout(()=>{
+  message = " I HAVE BEEN CHANGED"
+},2000);
+
+let doSomething = () => {
+  alert('DOOOO IT NOW')
+}
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+
+    <input type="text" v-model="message">
+    <p>{{message}}</p>
+
+    <button @click="doSomething">Click to alert
+    </button>
   </main>
 </template>

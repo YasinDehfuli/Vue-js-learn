34a34573dee77870782446eb8560e8f7e755ef82
YasinDehfuli <ydehfuli@gmail.com>
2023-10-14 11:07:15 +0330

    Add reactivityTransform option to vue plugin in vite.config.js.

--- Vue-Learn-Part-2/vite.config.js
+++ Vue-Learn-Part-2/vite.config.js
@@ -6,7 +6,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      reactivityTransform: true,
+    }),
   ],
   resolve: {
     alias: {

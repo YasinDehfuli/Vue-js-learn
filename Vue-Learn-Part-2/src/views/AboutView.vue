3abe361ab5cb8a7acf156efca3acbf4a28aa6a81
YasinDehfuli <ydehfuli@gmail.com>
2023-10-16 16:53:46 +0330

    Add flash functionality to the AboutView component.

--- Vue-Learn-Part-2/src/views/AboutView.vue
+++ Vue-Learn-Part-2/src/views/AboutView.vue
@@ -1,15 +1,13 @@
+<script setup>
+
+import {useFlash} from "@/composables/useFlash";
+
+let {flash} = useFlash();
+
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <button @click="flash('LALALLAL','HOOOOG JOIB NBABE', 'warning')">About Alert</button>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>

15c5e1525c176f2cefcee699abfdb6d0a94c2ecf
YasinDehfuli <ydehfuli@gmail.com>
2023-10-17 20:55:05 +0330

    Add FormView component to the router.

--- Vue-Learn-Part-2/src/router/index.js
+++ Vue-Learn-Part-2/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import FormView from "@/views/FormView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,7 +14,12 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+      path: '/form',
+      name: 'form',
+      component: () => import('../views/FormView.vue')
+    },
   ]
 })
 

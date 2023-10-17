49dad9d6fb50daf79fee514e98976cc516e682d5
YasinDehfuli <ydehfuli@gmail.com>
2023-10-17 20:56:01 +0330

    Add RouterLink to "/form" in the navigation bar.

--- Vue-Learn-Part-2/src/App.vue
+++ Vue-Learn-Part-2/src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/form">Form</RouterLink>
       </nav>
     </div>
   </header>

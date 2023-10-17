86e67016bd3423913adcf3f6c14df9a15b958763
YasinDehfuli <ydehfuli@gmail.com>
2023-10-17 20:56:15 +0330

    Add FormView.vue component with a form for inputting favorite food.

---
+++ Vue-Learn-Part-2/src/views/FormView.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+import {ref} from "vue";
+
+let food = ref('');
+
+// let age = ref('')
+
+function write(){
+  localStorage.setItem('food', food.value);
+}
+
+</script>
+
+<template>
+  <main>
+  <div class="about">
+    What is your favorite food ? <input type="text" v-model="food" @input="write">
+    <br>
+<!--    Whats Your Age ? <input type="text" v-model="age">-->
+  </div>
+  </main>
+</template>

2ac103a34a313a6f265141c32edf3d80812e1e58
YasinDehfuli <ydehfuli@gmail.com>
2023-10-20 14:24:09 +0330

    Add new file TextArea.vue with tab function and a TabbabaleTextarea component, including a ref for the comment and a setTimeout to change the comment value after 4 seconds.

---
+++ Vue-Learn-Part-2/src/views/TextArea.vue
@@ -0,0 +1,17 @@
+<!-- tab function -->
+<script setup>
+import TabbabaleTextarea from "@/components/TabbabaleTextarea.vue";
+import {ref} from "vue";
+
+let comment = ref('Please enter your text here');
+
+setTimeout(() => {
+  comment.value = 'It Works !!!'
+},4000)
+</script>
+
+<template>
+  <main>
+  <tabbabale-textarea v-model="comment" />
+  </main>
+</template>

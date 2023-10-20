c0bfee495b91a5ba432552874c1e166f6dcbb70a
YasinDehfuli <ydehfuli@gmail.com>
2023-10-20 14:23:22 +0330

    Add a RouterLink to navigate to the "/textarea" page.

--- Vue-Learn-Part-2/src/App.vue
+++ Vue-Learn-Part-2/src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/form">Form</RouterLink>
+        <RouterLink to="/textarea">TextArea</RouterLink>
       </nav>
     </div>
   </header>
